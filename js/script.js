fb91776eff098eef6c06987df1201b875de11a8b
Yash Agrawal <yagrawl2@gmail.com>
2017-09-05 13:57:53 -0500

    scrolling + dynamic

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
+// Scroll Reveal
+window.sr = ScrollReveal();
+sr.reveal('.landing-text');
+sr.reveal('.appointment-button', { duration: 2000, origin: 'bottom',  distance: '20px' });
+sr.reveal('.chair', { duration: 2000, origin: 'right',  distance: '80px' });
+sr.reveal('.haircutLeft', { duration: 1000, origin: 'bottom',  distance: '40px' });
+sr.reveal('.haircutRight', { duration: 1000, origin: 'bottom',  distance: '40px' });
+
+// Form submission actions
 function confirm(){
 	let appointment = document.getElementById('appointment');
-	let login = document.getElementById('login');
+	let book = document.getElementById('book');
 
-	appointment.removeChild(login);
+	appointment.removeChild(book);
 
 	var div = document.createElement('div');
     div.className = "thanks-container";
@@ -10,7 +19,7 @@ function confirm(){
     
     var p = document.createElement('p');
     p.className = "thanks";
-    p.textContent = "Our Llama specialists will reach out to for confirmation. We look forward to meeting your Llama!";
+    p.innerHTML = "Our Llama specialists will reach out to for confirmation.</br> We look forward to meeting your Llama!";
 
     div.appendChild(p);
     appointment.appendChild(div);
